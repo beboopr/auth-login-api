c03c58e5f9aa8ddb9e8e04783826e501c0efd05e
beboopr <linnets-router.09@icloud.com>
2022-11-04 15:57:28 -0400

    auth login api

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express  from "express";
 import cors from "cors"
+import { userLogin, addNewUser, updateUser} from "./src/users.js";
 const port = 3030
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.patch('/users/:uid', updateUser)
 
 app.post('/login', userLogin)
-app.post('/user', addNewUser)
-app.listen, port, () => console.log(`Listening to http://localhost${port}...`)
+app.post('/users', addNewUser)
+app.listen( port, () => console.log(`Listening to http://localhost${port}...`))
--- src/users.js
+++ src/users.js
@@ -1,11 +1,13 @@
-import dbConnect from "./dbConnect.js";
+import jwt from 'jsonwebtoken'
+import dbConnect from './dbConnect.js';
+import { secretKey } from '../secrets.js';
 
 export async function userLogin(req, res) {
   const { email, password } = req.body;
   const db = dbConnect();
   const matchingUsers = await db
     .collection("users")
-    .where("email", "==", email.toLowercase())
+    .where("email", "==", email.toLowerCase())
     .where("password", "==", password)
     .get()
     const users = matchingUsers.docs.map(doc => ({...doc.data(), uid: doc.id }))
@@ -14,5 +16,32 @@ export async function userLogin(req, res) {
         return
     }
     // if we get here, we have (at least) one matching user 
-    
+    let user = users[0]
+    user.password = undefined
+    const token = jwt.sign(user, secretKey)
+    res.send({user, token})
+}
+
+export async function addNewUser(req, res) {
+    const { email, password } = req.body
+    const db = dbConnect()
+    // w should check to see if email already in used
+    await db.collection('users').add({email: email.toLowerCase(), password})
+    userLogin(req, res)
+}
+
+export async function updateUser(req, res) {
+ const token = req.headers.authorization
+ const decodedToken = jwt.verify(token, secretKey)
+ const { uid } = req.params // profile they want to update
+if(uid !== decodedToken.uid) {
+    res.status(401).send({message: 'Invalid token ID' })
+    return
+}
+const db = dbConnect()
+await db.collection('users').doc(uid).update(rq.body)
+let user = doc.data()
+user.uid = doc.id
+user.password = undefined
+re.status(202).send({message: 'updated'})
 }
